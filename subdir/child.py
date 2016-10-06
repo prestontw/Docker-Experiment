32d15978f4131fd0b6ab304940247d072cb235a1
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2016-10-06 15:06:40 -0400

    get child to import parent

--- subdir/child.py
+++ subdir/child.py
@@ -1,6 +1,12 @@
 # want to import parent.py
-import "../parent.py"
+from pathlib import Path
+import sys
+p = Path('..').resolve()
+sys.path.append(str(p))
+print(sys.path)
+import parent
 
 print("will try to access parent's function")
-parent.parentFunction(2)
+parentResult = parent.parentFunction(2)
+print(parentResult)
 print("done")
